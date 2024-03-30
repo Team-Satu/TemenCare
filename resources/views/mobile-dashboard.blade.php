185e9c3060e22ea42a123c0b54ecf718b7d4b059
Alfian <alfiananandaputra@gmail.com>
2024-03-30 10:14:34 +0700

    heckpoint

--- resources/views/mobile-dashboard.blade.php
+++ resources/views/mobile-dashboard.blade.php
@@ -2,23 +2,21 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+    @googlefonts
     @vite('resources/css/app.css')
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Temen Care</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 </head>
 
 <body class="bg-gray-100">
     <div class="max-w-md mx-auto bg-green-50">
         <!-- Content -->
         <main class="bg-white h-screen">
-            <section class="w-full h-52 bg-gradient-to-r from-blue-400 to-blue-200">
-                <p>alfian</p>
+            <section class="w-full bg-gradient-to-r from-blue-400 to-blue-200 pt-16 px-6">
+                <h1 class="font-semibold text-white text-xl">Halo Holy!</h1>
+                <h1 class="font-medium text-white text-lg">Bagaimana Kondisimu Hari ini ?</h1>
             </section>
 
             <div class="bg-gradient-to-r from-green-100 to-green-900 bg-white shadow-lg rounded-lg p-6 w-44 h-44">
