624e027e20bb6e04d08b0b22ec6382671ce95dca
Alfian <alfiananandaputra@gmail.com>
2024-03-30 14:48:22 +0700

    style: each item component

--- resources/views/mobile-dashboard.blade.php
+++ resources/views/mobile-dashboard.blade.php
@@ -74,8 +74,27 @@
                         pertolongan dengan segera.</p>
 
                     <div>
-                        <div class="w-[165px]">
+                        <div class="w-48 shadow-md rounded-2xl mt-6">
+                            <div
+                                class="rounded-t-2xl h-36 w-full bg-[url(https://images.unsplash.com/photo-1651008376811-b90baee60c1f?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-center bg-cover">
+                            </div>
+                            <div class="bg-white w-full rounded-b-2xl py-2">
+                                <h1
+                                    class="text-center text-sky-600 text-xs font-semibold font-['Poppins'] capitalize leading-normal tracking-wide">
+                                    Insan Daud, M. Psi. </h1>
+                                <div
+                                    class="text-center text-neutral-600 text-[10px] px-2 font-normal font-['Poppins'] capitalize tracking-wide my-2">
+                                    Psikolog Industri & Organisasi <br />Spesialis Karir</div>
 
+                                {{-- Loop --}}
+                                <div class="px-2 my-1">
+                                    {{-- Each Component --}}
+                                    <div
+                                        class="px-[4px] py-[1px] bg-slate-50 rounded-3xl border border-sky-600 border-opacity-50 flex-col justify-center items-center inline-flex text-sky-600 text-[10px] font-medium font-['Poppins'] capitalize leading-normal tracking-wide">
+                                        Pekerjaan
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
