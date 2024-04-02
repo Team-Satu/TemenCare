bc8502ba77c4df7b72afca6337524f89c4876180
Alfian <alfiananandaputra@gmail.com>
2024-04-02 21:04:02 +0700

    style: icon

--- resources/views/components/bottom-menu.blade.php
+++ resources/views/components/bottom-menu.blade.php
@@ -1,1 +1,8 @@
-    <div class="h-20 w-full bg-red-500 sticky bottom-0 z-50"></div>
+<head>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <div class="h-20 w-full bg-red-500 sticky bottom-0 z-50">
+        <i class="fas fa-coffee"></i>
+    </div>
+</body>
