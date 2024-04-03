adb1edf79c5fff0b7540b5109d3d35f87fc27b84
Alfian <alfiananandaputra@gmail.com>
2024-04-04 00:55:20 +0700

    style: slicing front end

--- resources/views/login-igracias.blade.php
+++ resources/views/login-igracias.blade.php
@@ -6,24 +6,38 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Temen Care: Login</title>
+    <title>Temen Care</title>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    {{-- Fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
 </head>
 
-<body>
-    <form method="POST" action="/login" class="items-center justify-center flex flex-col h-screen bg-white space-y-2">
-        @csrf
+<body class="bg-gray-100">
+    <div class="max-w-md mx-auto bg-green-50 relative">
+        <!-- Content -->
+        <main class="bg-white h-screen flex">
+            <form method="POST" action="/login" class="items-center justify-center flex flex-col flex-1 px-6">
+                @csrf
+                <div class="space-y-4">
+                    <h1 class="text-neutral-600 text-3xl poppins-semibold text-center">Selamat Datang!</h1>
+                    <p class="text-center text-neutral-600 text-sm poppins-medium">Senang bertemu kembali!</p>
+                </div>
 
-        <input type="text" name="username" placeholder="Masukkan username Igracias Anda" class="border" />
-        <input type="password" name="password" placeholder="Masukkan password Igracias Anda" class="border" />
+                <div class="space-y-4 mb-8 mt-16">
+                    <input class="rounded border border-[#C4C4C4] w-full h-10 px-4 poppins-regular"
+                        placeholder="Masukkan Username Igracias Anda" type="text" name="username" />
+                    <input class="rounded border border-[#C4C4C4] w-full h-10 px-4 poppins-regular"
+                        placeholder="Masukkan Password Igracias Anda" type="password" name="password" />
+                </div>
 
-        <button>Masuk</button>
-        <!-- Equivalent to... -->
-        {{-- <input type="hidden" name="_token" value="{{ csrf_token() }}" /> --}}
-    </form>
+                <button class="bg-[#7EBAEB] h-10 w-full text-white poppins-semibold rounded">Masuk</button>
+            </form>
+        </main>
+    </div>
 </body>
 
 </html>
