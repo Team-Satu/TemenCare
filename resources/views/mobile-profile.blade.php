d6d127ed8ff7546cf47d42f77060fc64a46c9207
holyalin <amalinazahranip@gmail.com>
2024-04-04 07:17:38 +0700

    add: FE user profile done

--- resources/views/mobile-profile.blade.php
+++ resources/views/mobile-profile.blade.php
@@ -24,7 +24,7 @@
                         <!-- Arrow Icon -->
                         <div class="flex items-center justify-center">
                             <button type="button" class="fa-solid fa-arrow-left-long text-gray-600 text-xl absolute left-6 top-6"></button>
-                            <h1 class="text-[22px] text-[#4A4E50] font-semibold text-center my-6">
+                            <h1 class="text-[24px] text-[#4A4E50] font-semibold text-center my-6">
                                 Profil Anda
                             </h1>
                         </div>
@@ -35,22 +35,22 @@
                         <!-- Field -->
                         <div class="mb-5">
                             <!-- Nama -->
-                            <div class="relative mb-5 px-3 py-4 w-full border rounded border-black border-opacity-25">
+                            <div class="relative mb-6 px-3 py-4 w-full border rounded border-black border-opacity-25">
                                 <!-- Display field -->
                                 <div class="text-black text-opacity-86 text-xs font-normal">Winter</div>
                                 <!-- Floating label -->
-                                <label class="absolute -top-3 left-3 bg-white px-1 text-xs font-normal leading-4 tracking-tight">Nama</label>
+                                <label class="absolute -top-2 left-3 bg-white px-1 text-xs font-normal leading-4 tracking-tight text-[#666666]">Nama</label>
                             </div>
-                            <!-- Nompr Telepon -->
-                            <div class="relative mb-[304px] px-3 py-4 w-full border rounded border-black border-opacity-25">
+                            <!-- Nomor Telepon -->
+                            <div class="relative mb-[364px] px-3 py-4 w-full border rounded border-black border-opacity-25">
                                 <!-- Display field -->
                                 <div class="text-black text-opacity-86 text-xs font-normal">081395096058</div>
                                 <!-- Floating label -->
-                                <label class="absolute -top-3 left-3 bg-white px-1 text-xs font-normal leading-4 tracking-tight">Nomor Telepon</label>
+                                <label class="absolute -top-2 left-3 bg-white px-1 text-xs font-normal leading-4 tracking-tight text-[#666666]">Nomor Telepon</label>
                             </div>
                             <!-- "Keluar" Button -->
                             <div class="w-full px-6 flex justify-center mb-10">
-                                <button type="button" class="w-36 h-8 bg-[#F36464] rounded-full shadow flex items-center justify-center text-white text-xs font-medium tracking-wide">
+                                <button type="button" class="w-72 h-12 bg-[#F36464] rounded-full shadow flex items-center justify-center text-white text-[16px] font-medium tracking-wide">
                                 Keluar
                                 </button>
                             </div>
@@ -59,85 +59,9 @@
                 </div>
             </div>
         </main>
-
-        <!-- Footer -->
-        <footer class="px-6 py-4">
-            <p class="text-center text-gray-500 text-sm">
-            </p>
-        </footer>
-    </div>
+        <x-bottom-menu></x-bottom-menu>
+    </div>  
 </body>
 
 </html>
-
-
-
-
-<!-- <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    @vite('resources/css/app.css')
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profil Anda</title>
-</head> -->
-
-<!-- <body class="bg-gray-100">
-    <div class="max-w-md mx-auto h-screen"> -->
-        <!-- Header -->
-        <!-- <header class="bg-white shadow-md py-8 flex justify-between items-center">
-            <a href="#" class="text-blue-500">Back Icon</a> 
-            <h1 class="text-xl font-bold">Profil Anda</h1>
-            <div class="opacity-0">Placeholder</div>
-        </header> -->
-
-        <!-- Content -->
-        <!-- <main class="mt-0">
-            <div class="bg-white shadow-md rounded-md p-4"> -->
-                <!-- Profil picture -->
-                <!-- <div class="flex justify-center items-center py-4">
-                    <img class="rounded-full h-24 w-24 object-cover" src="/img/winter.jpg.png" alt="profile image">
-                </div> -->
-                <!-- Nama -->
-                <!-- <div class="mt-4">
-                    <label class="block text-sm font-semibold text-gray-700 mb-2">Nama</label>
-                    <div class="p-3 bg-gray-200 rounded-md">
-                        <p class="text-md font-semibold">Winter</p>
-                    </div>
-                </div> -->
-                <!-- Nomor Telepon -->
-                <!-- <div class="mt-4">
-                    <label class="block text-sm font-semibold text-gray-700 mb-2">Nomor Telepon</label>
-                    <div class="p-3 bg-gray-200 rounded-md">
-                        <p class="text-md font-semibold">081385096058</p>
-                    </div>
-                </div> -->
-                <!-- Tombol Keluar -->
-                <!-- <button class="mt-20 w-full bg-red-500 text-white rounded-lg p-2 text-md font-semibold">Keluar</button>
-            </div>
-        </main> -->
-
-        <!-- Footer -->
-        <!-- <footer class="text-center text-gray-500 text-sm mt-4">
-            <div class="flex justify-around p-4"> -->
-                <!-- Placeholder untuk ikon-ikon -->
-                <!-- <a href="#" class="text-center text-gray-400">
-                    <div>Home Icon</div>
-                    <div class="text-xs">Home</div>
-                </a>
-                <a href="#" class="text-center text-gray-400">
-                    <div>History Icon</div>
-                    <div class="text-xs">History</div>
-                </a>
-                <a href="#" class="text-center text-blue-500">
-                    <div>Profile Icon</div>
-                    <div class="text-xs">Profile</div>
-                </a>
-            </div>    
-            <p>&copy; 2024 Mobile View Footer</p>
-        </footer>
-    </div>
-</body>
-
-</html> -->
+</html>
