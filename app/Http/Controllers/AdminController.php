bc1973f4e28576bd811a4d805efe62074e502d19
Alfian <alfiananandaputra@gmail.com>
2024-04-07 10:43:01 +0700

    feat: login admin and psycholog

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,16 +2,65 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Admin;
+use App\Models\Psychologs;
 use Illuminate\Http\Request;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class AdminController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return view('admin-login');
+    }
+
+    public function login(Request $request)
+    {
+        try {
+            /**
+             * Karena berbeda role, maka akan dilakukan pengecekan
+             * di kedua tabel. Yakni tabel psychologs dan admin.
+             */
+
+            // Admin
+            $credential = $request->only('email', 'password', '_token');
+
+            $email = $credential['email'];
+            $password = $credential['password'];
+
+            if (empty($email) || empty($password)) {
+                Alert::error('Harap Isi!', 'Email atau password harus diisi!');
+                return redirect()->back();
+            }
+
+            $countAdmin = Admin::where('email', $email)->count();
+
+            // Check if admin exist
+            if ($countAdmin) {
+                $user = Admin::where("email", $email)->first();
+            } else {
+                // Admin doesnt exist
+                $countPsycholog = Psychologs::where('email', $email)->count();
+
+                if ($countPsycholog) {
+                    $user = Psychologs::where('email', $email)->first();
+                } else {
+                    Alert::error('Gagal', 'Akun tidak ditemukan!');
+                    return redirect()->back();
+                }
+
+
+                // Create token
+                $token = $user->createToken("temen_token");
+                $plainTextToken = $token->plainTextToken;
+                $cookie = cookie('temen_cookie', $plainTextToken, 60 * 24 * 30);
+
+                return redirect(route("admin.dashboard"))->cookie($cookie);
+            }
+        } catch (\Throwable $th) {
+            Alert::error('Gagal', 'Terjadi masalah dengan akun Anda!');
+            return redirect()->back();
+        }
     }
 
     /**
