64463e6926b3c5cf0ea9b4d79a6f51d2d2fd3753
Alfian <alfiananandaputra@gmail.com>
2024-04-08 13:19:32 +0700

    style: loading when load page

--- resources/js/temen-care.js
+++ resources/js/temen-care.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
+    const loading = "<h2>Loading...</h2>"
+    $("#load-page").html(loading);
     $("#load-page").load("/admin/load/dashboard");
-
+    
     $("a.collapse-item").on("click", function () {
+        $("#load-page").html(loading);
         var targetLoad = $(this).attr('load');
         $("#load-page").load("/admin/"+targetLoad);
     });
