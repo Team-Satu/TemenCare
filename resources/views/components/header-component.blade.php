767093b2a0b7932da765f6e87e38a14bddb31277
Alfian <alfiananandaputra@gmail.com>
2024-04-15 23:57:44 +0700

    style: refactor styling

--- resources/views/components/header-component.blade.php
+++ resources/views/components/header-component.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-row flex-1 bg-white px-4 py-4 items-center w-full">
+<div class="flex flex-row flex-1 bg-white px-4 py-4 items-center w-full top-0 sticky">
     <i class="fa-solid fa-arrow-left"></i>
     <h1
         class="text-neutral-600 text-base poppins-semibold capitalize leading-normal tracking-wide flex flex-1 justify-center">
