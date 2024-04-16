f0b3f5777fcfd4f3257fc9ae797710916bc8ed10
Alfian <alfiananandaputra@gmail.com>
2024-04-16 07:53:27 +0700

    add: back component

--- resources/views/components/header-component.blade.php
+++ resources/views/components/header-component.blade.php
@@ -1,5 +1,7 @@
 <div class="flex flex-row flex-1 bg-white px-4 py-4 items-center w-full top-0 sticky">
-    <i class="fa-solid fa-arrow-left"></i>
+    <a href="{{ URL::previous() }}">
+        <i class="fa-solid fa-arrow-left"></i>
+    </a>
     <h1
         class="text-neutral-600 text-base poppins-semibold capitalize leading-normal tracking-wide flex flex-1 justify-center">
         {{ $title }}</h1>
