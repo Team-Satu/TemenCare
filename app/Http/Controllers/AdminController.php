d64e8e6e86b41de6e35574c9a9594cdb29aa2580
sabithaaulia <sabithaaulia@student.telkomuniversity.ac.id>
2024-05-08 21:39:30 +0700

    checkpoint: fe change schedules 2

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -149,19 +149,21 @@ class AdminController extends Controller
     public function showCommunities()
     {
         return view("admin-load.desktop-communities");
-
-    public function viewSchedules(){
-        return view("admin-load.view-schedules");
     }
 
-
-
-
-
+    public function viewSchedules()
+    {
+        return view("admin-load.view-schedules");
+    }
 
     public function showSchedule()
     {
         return view("admin-load.psycholog-schedules");
+    }
+
+    public function changeSchedule()
+    {
+        return view("admin-load.change-schedules");
     }
 
     public function showRegisterPsycholog(Request $request)
