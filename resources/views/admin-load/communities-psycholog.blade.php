3b19ac003f8f71f4cd3ee6998f2a7f363f96af33
InsanTaufik <insantaufik82@gmail.com>
2024-05-13 11:17:57 +0700

    checkpoint

--- resources/views/admin-load/communities-psycholog.blade.php
+++ resources/views/admin-load/communities-psycholog.blade.php
@@ -1,9 +1,72 @@
 <head>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+        /* Your CSS styles here */
+        .col-md-4 {
+            width: 410px;
+        }
+    </style>
 </head>
 
 <div class="container-fluid">
     <p class="text-start">Komunitas</p>
     <p class="h1">Rajin Meditasi Pikiran Damai</p>
-    
+    <div class="d-grid gap-4">
+        <div>
+            <div style="width: 530px; height: 271px; position: relative">
+                <img style="width: 758.02px; height: 348.55px; left: -104.77px; top: -24.11px" src="https://via.placeholder.com/758x349" />
+            </div>
+        </div>
+        <div style="padding-top: 60px; width: 550px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+        <div class="pt-1"><h1>Postingan</h1></div>
+        <div class="container ms-0">
+        <div class="row row-cols-2">
+            <div class="col-md-4 mb-4 ">
+                <div class="card" style="width: 24rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+            <div class="card" style="width: 24rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+            <div class="w-100"></div>
+            <div class="col-md-4">
+            <div class="card" style="width: 24rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+            <div class="card" style="width: 24rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
 </div>
