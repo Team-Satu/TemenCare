360786d73e8c5b5d0e4f0526fb6df45e5a85bae6
holyalin <amalinazahranip@gmail.com>
2024-06-05 22:32:40 +0700

    checkpoint : logout user

---
+++ tests/Browser/LogoutUserTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LogoutUserTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     * @group auth
+     */
+    public function testExample(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                    ->type('username', 'amalinalin')
+                    ->type('password', '5tr@yKids')
+                    ->press('Masuk')
+                    ->assertPathIs('/dashboard');
+        });
+    }
+}
