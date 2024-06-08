55c591233522f6b0d97ab2a19a75d809db71c717
Alfian <alfiananandaputra@gmail.com>
2024-06-08 23:13:50 +0700

    checkpoint

--- routes/web.php
+++ routes/web.php
@@ -196,7 +196,7 @@ Route::middleware(EnsureAdminTemenTokenCookieIsValid::class)->prefix("admin")->g
     Route::get("/create-expertise", [AdminController::class, 'showCreateExpertise'])->name('admin.show-create-expertise');
     Route::post("/create-expertise", [AdminController::class, 'createExpertise'])->name('admin.create-expertise');
     Route::get("/list-expertise", [AdminController::class, 'showListExpertise'])->name('admin.show-list-expertise');
-    Route::delete("/delete-expertise", [AdminController::class, 'deleteExpertise'])->name('admin.delete-expertise');
+    Route::delete("/delete-expertise/{expertise_id}", [AdminController::class, 'deleteExpertise'])->name('admin.delete-expertise');
 });
 
 // Only admin load purpose - Authenticated
