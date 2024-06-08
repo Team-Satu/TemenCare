5a94e0404b8e6efc3a182b2ae5b930a3a3533df9
WnnDk <windykurniawan14@gmail.com>
2024-06-09 01:26:26 +0700

    test:passed register kenalan test

---
+++ tests/Browser/RegisterKenalanTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class RegisterKenalanTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     */
+    public function testExample(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                    ->type('username', 'sabithaaulia')
+                    ->type('password', 'Bulans@b1t')
+                    ->press('Masuk')
+                    ->assertPathIs('/dashboard')
+                    ->clicklink('Kenalan')
+                    ->type('whatsapp_number', '081288421240')
+                    ->press('Simpan & Setuju');
+        });
+    }
+}
