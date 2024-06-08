d741e38c239b0ebd0c6f8b1fbb011d0dcd33ab97
Alfian <alfiananandaputra@gmail.com>
2024-06-08 22:37:55 +0700

    feat: create expertise

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -94,8 +94,8 @@
             <div id="collapseExpertise" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                 <div class="bg-white py-2 collapse-inner rounded">
                     <h6 class="collapse-header">Atur Expertise:</h6>
-                    <a class="collapse-item" href="{{ route('admin.show-create-expertise') }}">Tambah Profile</a>
-                    <a class="collapse-item" href="{{ route('admin-load.show-list-expertise') }}">Daftar Profile</a>
+                    <a class="collapse-item" href="{{ route('admin.show-create-expertise') }}">Tambah Expertise</a>
+                    <a class="collapse-item" href="{{ route('admin-load.show-list-expertise') }}">Daftar Expertise</a>
                 </div>
             </div>
         </li>
