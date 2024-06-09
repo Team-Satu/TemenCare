80d6fdd44ffbd1f1bbf41a250e7a78d0a0f13388
WnnDk <windykurniawan14@gmail.com>
2024-06-09 15:19:09 +0700

    test:passed sign out kenalan test

---
+++ tests/Browser/SignOutKenalanTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class SignOutKenalanTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     */
+    public function testExample(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                    ->type('username', 'sabithaaulia')
+                    ->type('password', 'Bulans@b1t')
+                    ->press('Masuk')
+                    ->assertPathIs('/dashboard')
+                    ->clicklink('Kenalan')
+                    ->click('a[href="http://127.0.0.1:8000/kenalan/profile"]')
+                    ->press('Keluar dari Kenalan');
+        });
+    }
+}
