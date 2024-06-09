64cd20da0ce36cc5f1761b67a36577942aa054a4
sabithaaulia <sabithaaulia@student.telkomuniversity.ac.id>
2024-06-09 18:07:32 +0700

    test: passed see psikolog expertise

---
+++ tests/Browser/SeePsikologExpertiseTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class SeePsikologExpertiseTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     */
+    public function testExample(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/admin')
+            ->type('email','sabitha@email.com')
+            ->type('password','sabitha123')
+            ->press('Login')
+            ->assertPathIs('/admin/dashboard')
+            ->click('.nav-link.collapsed[data-target="#collapseExpertise"]')
+            ->pause(1000)
+            ->click('.collapse-item[href="http://127.0.0.1:8000/admin/list-expertise"]');
+        });
+    }
+}
